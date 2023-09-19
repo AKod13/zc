70af01493bf14344ddba7e9d769ce91d3620524c
Akod <akodzoman13@gmail.com>
2023-09-19 15:28:26 +0200

    task_4 setup

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Zc
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
+    
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
@@ -27,3 +27,4 @@ module Zc
     config.api_only = true
   end
 end
+
